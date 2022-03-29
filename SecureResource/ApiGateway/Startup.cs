e70e14539dd8b15bdc76ea06fa83580ec0426deb
agung <agung@drosha.com>
2022-03-29 21:26:43 +0700

    first check

--- SecureResource/ApiGateway/Startup.cs
+++ SecureResource/ApiGateway/Startup.cs
@@ -18,7 +18,7 @@ namespace ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
+        { //test
             var authenticationProviderKey = "IdentityApiKey";
 
             // NUGET - Microsoft.AspNetCore.Authentication.JwtBearer
